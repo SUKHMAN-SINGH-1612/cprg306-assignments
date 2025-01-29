2bdee9a81e63f25478d7163a4ed60824aeb60f8f
Sukhman Singh <sukhmansinghbhogal@gmail.com>
2025-01-29 13:14:53 -0700

    weel 4

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ export default function Page() {
                 <ul>
                   <li className="hover:text-green-400 hover:underline"><a href = 'week-2'>Week 2 Assignment</a></li>
                   <li className="hover:text-green-400 hover:underline"><a href = 'week-3'>Week 3 Assignment</a></li>
+                  <li className="hover:text-green-400 hover:underline"><a href = 'week-4'>Week 4 Assignment</a></li>
                 </ul>
               </div>
             </div>
